00d1b48b4025da5a22ac094e5093021cadbc8674
Hoderick <rikkih@protonmail.com>
2025-05-27 18:56:05 +0100

    auth0 bits

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="REDACTED"
-      clientId="REDACTED"
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: "https://REDACTED/api/v2/",
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       }}
     >
       <App />
