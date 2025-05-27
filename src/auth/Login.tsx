250fbc37ad79cb4178726a890c9ae7dd49fca4e1
Hoderick <rikkih@protonmail.com>
2025-05-27 18:42:50 +0100

    centralise login page

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,21 +1,32 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 const LoginPage: React.FC = () => {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <Container>
-      <Typography variant="h5" gutterBottom>
-        Sign in to Rabbit Hole
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        Click below to log in via Auth0.
-      </Typography>
-      <Button variant="contained" onClick={() => loginWithRedirect()}>
-        Log In
-      </Button>
-    </Container>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "background.default", // optional, for background color
+        px: 2, // optional padding for small screens
+      }}
+    >
+      <Container>
+        <Typography variant="h5" gutterBottom>
+          Sign in to Rabbit Hole
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 2 }}>
+          Click below to log in via Auth0.
+        </Typography>
+        <Button variant="contained" onClick={() => loginWithRedirect()}>
+          Log In
+        </Button>
+      </Container>
+    </Box>
   );
 };
 
